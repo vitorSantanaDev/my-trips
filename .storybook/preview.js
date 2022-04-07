b9db30d04fbce7c2b3b6299dc90495bee792275f
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-04-07 20:37:47 -0300

    ci: :seedling: improving generators and storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
+
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
+import { theme } from '../src/styles/theme/theme'
 
 export const parameters = {
   nextRouter: {
@@ -9,9 +12,9 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
